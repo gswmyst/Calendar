56f74f8b24a87792ee31e54114571b9f6c1a24b6
YeShantao <yeshantao@nbdeli.com>
2020-12-17 13:47:26 +0800

    open setting activity at first open

--- app/src/main/java/com/yst/calendar/MainActivity.kt
+++ app/src/main/java/com/yst/calendar/MainActivity.kt
@@ -5,15 +5,28 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDate
+import java.time.Period
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sp:SharedPreferences=this.getSharedPreferences("preference",Context.MODE_PRIVATE)
-        if (sp.getString("myname","").equals(""))
+        val sp:SharedPreferences=this.getSharedPreferences("preference",Context.MODE_PRIVATE)//sharedpreference对象
+        val strmyname=sp.getString("myname","")//获取sharedpreference中我的称呼
+        if (strmyname.equals(""))//如果没有我的称呼则去设置activity
         {
-            startActivity(Intent(this,SettingActivity::class.java))
+            startActivity(Intent(this,SettingActivity::class.java))//打开设置activity
+            finish()//关闭这个activity
         }
+        val strlovename=sp.getString("lovername","")//获取对方的称呼
+        val startdatestr=sp.getString("date","")//获取开始时间
+        val startdate=LocalDate.parse(startdatestr)//开始时间string转化为localdate
+        val today=LocalDate.now()//获取当前日期
+        //val period=Period.between(startdate,today)//获取period,包含年月日
+        val days=today.toEpochDay()-startdate.toEpochDay()+1//计算相差天数
+        textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+days+"天"//输出
     }
 }
--- app/src/main/java/com/yst/calendar/SettingActivity.kt
+++ app/src/main/java/com/yst/calendar/SettingActivity.kt
@@ -1,11 +1,50 @@
 package com.yst.calendar
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        button.isEnabled=false
+        MyName.addTextChangedListener(object :TextWatcher{//edittext textchanged消息监听器
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable){
+                button.isEnabled = !(MyName.length()==0||LoversName.length()==0)//禁用/启用按钮
+            }
+        })
+        LoversName.addTextChangedListener(object :TextWatcher{//edittext textchanged消息监听器
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable){
+                button.isEnabled = !(MyName.length()==0||LoversName.length()==0)//禁用/启用按钮
+            }
+        })
+        button.setOnClickListener{//按钮消息监听器
+            val sp: SharedPreferences =this.getSharedPreferences("preference", Context.MODE_PRIVATE)//sharedpreference对象
+            val editor:SharedPreferences.Editor=sp.edit()
+            editor.putString("myname", MyName.text.toString())//添加双方称呼
+            editor.putString("lovername",LoversName.text.toString())
+            var month=(datePicker.month+1).toString()//获取设置的日期，若月份和天数小于10则前面加上0
+            if (month.length==1)
+                month="0"+month
+            var day=datePicker.dayOfMonth.toString()
+            if (day.length==1)
+                day="0"+day
+            val str=datePicker.year.toString()+"-"+month+"-"+day//设置为iso标准日期格式
+            editor.putString("date",str)//添加日期
+            if (editor.commit()) {//提交后打开MainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()//关闭这个activity
+            }
+        }
     }
 }
