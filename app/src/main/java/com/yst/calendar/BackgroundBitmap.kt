de6086d192d792e6256c70063cc5a1cca0fc7ec4
YeShantao <yeshantao@nbdeli.com>
2021-01-08 13:22:42 +0800

    fixed a bug when press switch button the number of days is not accurate. It can now scale the background image automatically

---
+++ app/src/main/java/com/yst/calendar/BackgroundBitmap.kt
@@ -0,0 +1,70 @@
+package com.yst.calendar
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.net.Uri
+import androidx.exifinterface.media.ExifInterface
+import com.zhuolong.bitmaphelper.BitmapHelperFactory
+import com.zhuolong.bitmaphelper.shape.BitmapShapeOption
+import kotlinx.android.synthetic.main.activity_main.*
+
+class BackgroundBitmap(private val activity: MainActivity, uri: Uri) {
+    var bitmap: Bitmap
+
+    init {
+        //初始化时对图像进行压缩,再给bitmap赋值
+        var input = activity.contentResolver.openInputStream(uri)
+        val option = BitmapFactory.Options()
+        option.inJustDecodeBounds = true
+        BitmapFactory.decodeStream(input, null, option)
+        input!!.close()
+        val originalWidth = option.outWidth
+        val originalHeight = option.outHeight
+        val width = activity.cl.width
+        val height = activity.cl.height
+        option.inJustDecodeBounds = false
+        input = activity.contentResolver.openInputStream(uri)
+        if (originalWidth > width && originalHeight > height) {
+            //如果图片宽高均大于手机分辨率对图片进行压缩
+            var ratio = 1
+            ratio = if ((originalHeight / height) > (originalWidth / width)) {
+                originalWidth / width;
+            } else {
+                originalHeight / height
+            }
+            option.inSampleSize = ratio
+            bitmap = BitmapFactory.decodeStream(input, null, option)!!
+        } else {
+            //如果图片宽高有一个小于手机分辨率对图片进行拉伸
+            val ratio: Float =
+                if ((height.toFloat() / originalHeight.toFloat()) > (width.toFloat() / originalWidth.toFloat())) {
+                    height.toFloat() / originalHeight.toFloat()
+                } else {
+                    width.toFloat() / originalWidth.toFloat()
+                }
+            val newHeight: Int = (originalHeight * ratio).toInt()
+            val newWidth: Int = (originalWidth * ratio).toInt()
+            bitmap = BitmapFactory.decodeStream(input, null, option)!!
+            bitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
+        }
+        input!!.close()
+
+        //获取图片信息，如有旋转处理旋转
+        val exifInterface = ExifInterface(activity.contentResolver.openInputStream(uri)!!)
+        val mat = Matrix()
+        when (exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> mat.postRotate(90F)
+            ExifInterface.ORIENTATION_ROTATE_180 -> mat.postRotate(180F)
+        }
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, mat, true)
+    }
+
+    fun cut() {
+        val rect = Rect(0, 0, activity.cl.width, activity.cl.height)//设置裁剪的长宽
+        val option = BitmapShapeOption.Builder().build()
+        val helper = BitmapHelperFactory.newBitmapShapeHelper()
+        bitmap = helper.bitmapRectShapeHelper.clipRectShapeInCenter(bitmap, rect, option)
+    }
+}
--- app/src/main/java/com/yst/calendar/MainActivity.kt
+++ app/src/main/java/com/yst/calendar/MainActivity.kt
@@ -6,19 +6,12 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
-import com.nanchen.compresshelper.CompressHelper
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.time.LocalDate
 import java.time.Period
-import androidx.core.app.ActivityCompat
-
-import android.content.pm.PackageManager
-
-import android.app.Activity
 import android.graphics.BitmapFactory
 import android.text.TextUtils
 import java.io.FileOutputStream
@@ -26,74 +19,69 @@ import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
-    private val imagecode=1//选择图片的request code
-    private val REQUEST_EXTERNAL_STORAGE=1//获取存储权限参数
-    private val PERMISSIONS_STORAGE = arrayOf(
-        "android.permission.READ_EXTERNAL_STORAGE",
-        "android.permission.WRITE_EXTERNAL_STORAGE"
-    )
+    private val IMAGE_CODE = 1//选择图片的request code
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        verifyStoragePermissions(this)
-        val sp:SharedPreferences=this.getSharedPreferences("preference", MODE_PRIVATE)//sharedpreference对象
-        val strmyname=sp.getString("myname","")//获取sharedpreference中我的称呼
+        val sp: SharedPreferences = this.getSharedPreferences("preference", MODE_PRIVATE)//sharedpreference对象
+        val strmyname = sp.getString("myname", "")//获取sharedpreference中我的称呼
         if (strmyname.equals(""))//如果没有我的称呼则去设置activity
         {
-            startActivity(Intent(this,SettingActivity::class.java))//打开设置activity
+            startActivity(Intent(this, SettingActivity::class.java))//打开设置activity
             finish()//关闭这个activity
         }
-        val strlovename=sp.getString("lovername","")//获取对方的称呼
-        val startdatestr=sp.getString("date","")//获取开始时间
-        val startdate=LocalDate.parse(startdatestr)//开始时间string转化为localdate
-        val today=LocalDate.now()//获取当前日期
-        val period=Period.between(startdate,today)//获取period,包含年月日
-        var days=today.toEpochDay()-startdate.toEpochDay()+1//计算相差天数
-        textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+days+"天"//输出
-        val path=getExternalFilesDir("background/background.jpg").toString()//图片路径
-        if (!TextUtils.isEmpty(path)){//路径不存在则不加载图片
-            val bitmap=BitmapFactory.decodeFile(path)
+        val strlovename = sp.getString("lovername", "")//获取对方的称呼
+        val startdatestr = sp.getString("date", "")//获取开始时间
+        val startdate = LocalDate.parse(startdatestr)//开始时间string转化为localdate
+        val today = LocalDate.now()//获取当前日期
+        val period = Period.between(startdate, today)//获取period,包含年月日
+        val days = today.toEpochDay() - startdate.toEpochDay() + 1//计算相差天数
+        textview.text = "今天是" + strmyname + "和" + strlovename + "在一起的第" + days + "天"//输出
+        val path = getExternalFilesDir("background/background.jpg").toString()//图片路径
+        if (!TextUtils.isEmpty(path)) {//路径不存在则不加载图片
+            val bitmap = BitmapFactory.decodeFile(path)
             try {//判断图片是否正确加载
                 bitmap.width
-            }catch (e:Exception){}
-            val drawable=BitmapDrawable(resources,bitmap)//设置背景图片
-            cl.background=drawable
-            cl.background.alpha=50
-        }
-        switch1.setOnCheckedChangeListener{switch1,isChecked->//切换天数格式
-            if (isChecked){
-                switch1.text=getString(R.string.yy_mm_dd)
-                days= (period.days+1).toLong()
-                if (period.years==0 && period.months==0)
-                    textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+days+"天"
-                else if (period.years==0)
-                    textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+period.months+"月"+days+"天"
-                else if (period.months==0)
-                    textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+period.years+"年"+days+"天"
-                else
-                    textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+period.years+"年"+period.months+"月"+days+"天"
+            } catch (e: Exception) {
             }
-            else{
-                switch1.text=getString(R.string.days)
-                textview.text="今天是"+strmyname+"和"+strlovename+"在一起的第"+days+"天"
+            val drawable = BitmapDrawable(resources, bitmap)//设置背景图片
+            cl.background = drawable
+            cl.background.alpha = 50
+        }
+        switch1.setOnCheckedChangeListener { switch1, isChecked ->//切换天数格式
+            if (isChecked) {
+                switch1.text = getString(R.string.yy_mm_dd)
+                val day = (period.days + 1).toLong()
+                if (period.years == 0 && period.months == 0)
+                    textview.text = "今天是" + strmyname + "和" + strlovename + "在一起的第" + day + "天"
+                else if (period.years == 0)
+                    textview.text = "今天是" + strmyname + "和" + strlovename + "在一起的第" + period.months + "月" + day + "天"
+                else if (period.months == 0)
+                    textview.text = "今天是" + strmyname + "和" + strlovename + "在一起的第" + period.years + "年" + day + "天"
+                else
+                    textview.text =
+                        "今天是" + strmyname + "和" + strlovename + "在一起的第" + period.years + "年" + period.months + "月" + day + "天"
+            } else {
+                switch1.text = getString(R.string.days)
+                textview.text = "今天是" + strmyname + "和" + strlovename + "在一起的第" + days + "天"
             }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {//添加菜单
-        menu?.add(Menu.NONE,0,0,"重新设置")
-        menu?.add(Menu.NONE,1,1,"选择背景图片")
+        menu?.add(Menu.NONE, 0, 0, "重新设置")
+        menu?.add(Menu.NONE, 1, 1, "选择背景图片")
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){//判断打开的是哪个菜单项
-            0 -> startActivity(Intent(this,SettingActivity::class.java))//点击重新设置菜单打开设置activity
-            1->{//点击选择图片打开相册
+        when (item.itemId) {//判断打开的是哪个菜单项
+            0 -> startActivity(Intent(this, SettingActivity::class.java))//点击重新设置菜单打开设置activity
+            1 -> {//点击选择图片打开相册
                 val getAlbum = Intent(Intent.ACTION_PICK)
                 getAlbum.type = "image/*"
-                startActivityForResult(getAlbum,imagecode)
+                startActivityForResult(getAlbum, IMAGE_CODE)
             }
         }
         return super.onOptionsItemSelected(item)
@@ -101,63 +89,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {//获取得到的图片路径并保存
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode!= RESULT_OK)
+        if (resultCode != RESULT_OK)
             return
-        if (requestCode==imagecode){
+        if (requestCode == IMAGE_CODE) {
             val originalURI = data!!.data!!
-            val filePathColumn= arrayOf(MediaStore.Images.Media.DATA)
-            val cursor = contentResolver.query(originalURI,filePathColumn,null,null,null)
-            if (cursor!=null){
-                cursor.moveToFirst()
-                val columnIndex=cursor.getColumnIndexOrThrow(filePathColumn[0])
-                val path=cursor.getString(columnIndex)
-                val file = File(path)
-                if (!file.exists())
-                    return
-                val bitmap= CompressHelper.Builder(this)
-                    .setMaxWidth(1080F)
-                    .setMaxHeight(1920F)
-                    .setQuality(80)
-                    .setFileName("background.jpg")
-                    .setCompressFormat(Bitmap.CompressFormat.JPEG)
-                    .setDestinationDirectoryPath(getExternalFilesDir(null).toString())
-                    .build()
-                    .compressToBitmap(file)
-                val drawable=BitmapDrawable(resources,bitmap)
-                cl.background=drawable
-                cl.background.alpha=50
-                saveBitmap(bitmap)
-            }
+            val backgroundBitmap=BackgroundBitmap(this,originalURI)
+            backgroundBitmap.cut()
+            val drawable = BitmapDrawable(resources, backgroundBitmap.bitmap)
+            cl.background = drawable
+            cl.background.alpha = 50
+            saveBitmap(backgroundBitmap.bitmap)
         }
     }
 
     private fun saveBitmap(bitmap: Bitmap) {//保存图片到data/com.yst.Calendar/files/background
-        val appDir=File(getExternalFilesDir(null),"background")
+        val appDir = File(getExternalFilesDir(null), "background")
         if (!appDir.exists())
             appDir.mkdir()
-        val fileName="background"+".jpg"
-        val file=File(appDir,fileName)
-        if (file.exists()){
+        val fileName = "background" + ".jpg"
+        val file = File(appDir, fileName)
+        if (file.exists()) {
             file.delete()
             file.createNewFile()
         }
-        val fos=FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos)
+        val fos = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
         fos.flush()
         fos.close()
-    }
-
-    private fun verifyStoragePermissions(activity: Activity?) {//获取存储权限函数
-        try {
-            //检测是否有写的权限
-            val permission = ActivityCompat.checkSelfPermission(activity!!, "android.permission.WRITE_EXTERNAL_STORAGE")
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                // 没有写的权限，去申请写的权限，会弹出对话框
-                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
     }
 }
 
